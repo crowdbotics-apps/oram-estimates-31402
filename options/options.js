31ec160872d064a6ba1eb2390a13fcc7a65d54b6
Crowdbotics <team@crowdbotics.com>
2021-10-13 23:59:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "oram_estimates_31402",
+  url: "https://oram_estimates_31402.botics.co",
+  api: "https://oram_estimates_31402.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
